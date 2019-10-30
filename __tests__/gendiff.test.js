087e3c83f2c338fb5d710dd0dbaa57eb3daef94b
shapurid <i.v.korney@yandex.ru>
2019-10-30 12:09:50 +0300

    Pre-Step 2: add comment

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -3,6 +3,6 @@ import genDiff from '../src';
 
 test('genDiff', () => {
   const received = genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json');
-  const result = fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf8');
+  const result = fs.readFileSync('./__tests__/__fixtures__/result.txt', 'utf8'); // не понимаю, почему fs.readDileSync() импортирует содержимое файла с отступом, есть ли возможность его убрать?
   expect(received).toBe(result);
 });
